95cd9de2493de9ceeb3e4687a6bc55369fa8ce21
Steedlan <arnaud.schellekens@student.vinci.be>
2024-12-05 13:42:38 +0100

    header

--- frontEnd/src/app/question/question.component.ts
+++ frontEnd/src/app/question/question.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-question',
+  imports: [HeaderComponent],
   templateUrl: './question.component.html',
   styleUrls: ['./question.component.css'],
 })
