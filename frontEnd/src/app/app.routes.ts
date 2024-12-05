87180afb59ad7a9d458c308cc0dfd8b35a3570d8
Steedlan <arnaud.schellekens@student.vinci.be>
2024-12-05 13:35:21 +0100

    premiere version question

--- frontEnd/src/app/app.routes.ts
+++ frontEnd/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { provideRouter } from '@angular/router';
+import { QuestionComponent } from './question/question.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/homepage', pathMatch: 'full' },
     { path: 'homepage', component: HomepageComponent },
     //{ path: 'login', component: LoginComponent }, pour rafael ca
+    { path:'esg/question/:id', component: QuestionComponent }
     
   ];
   
