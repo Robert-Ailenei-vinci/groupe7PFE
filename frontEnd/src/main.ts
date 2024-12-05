7d0903365f4fb1b1c3f086ce1d22fdc7611a9eac
Robert <robert.ailenei@student.vinci.be>
2024-12-05 11:51:05 +0100

    made router and by default you land on homepage

--- frontEnd/src/main.ts
+++ frontEnd/src/main.ts
@@ -2,7 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
+import { appRoutingProviders } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient()] // Fournir HttpClient
-}).catch((err) => console.error(err));
+  providers: [
+    appRoutingProviders,
+    provideHttpClient()
+  ]
+})
+.catch(err => console.error(err));
