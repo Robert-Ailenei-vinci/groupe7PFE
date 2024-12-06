569e35f24a76113a7ef60bce866841eaa0d8a7ef
Simon <simon.bosseler@student.vinci.be>
2024-12-06 14:18:34 +0100

    finish feature validate and finish questionnaire

--- backend/src/main/java/vinci/be/backend/controller/repondu/QuestionnaireReponduController.java
+++ backend/src/main/java/vinci/be/backend/controller/repondu/QuestionnaireReponduController.java
@@ -5,8 +5,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,7 +27,7 @@ public class QuestionnaireReponduController {
   }
 
 
-  @PostMapping("/create/questionnaireESG")
+  @PostMapping("/questionnaires/create/questionnaireESG")
   public ResponseEntity<QuestionnaireRepondu> createQuestionnaireESG(@RequestBody Map<String, String> body) {
     String idClient = body.get("idClient");
     QuestionnaireRepondu questionnaire = questionnaireReponduService.createOneESG(idClient);
@@ -36,9 +38,31 @@ public class QuestionnaireReponduController {
     return new  ResponseEntity<>(questionnaire, HttpStatus.CREATED);
   }
 
+
   @GetMapping("/questionnaires/{idClient}")
   public ResponseEntity<QuestionnaireRepondu> getQuestionnaireByIdClient(@PathVariable String idClient) {
     QuestionnaireRepondu questionnaire = questionnaireReponduService.getQuestionnaireByIdClient(idClient);
+
+    if (questionnaire == null) {
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    return new ResponseEntity<>(questionnaire, HttpStatus.OK);
+  }
+
+
+  @PatchMapping("/questionnaires/validate/{idQuestionnaire}")
+  public ResponseEntity<QuestionnaireRepondu> validateQuestionnaire(@PathVariable String idQuestionnaire) {
+    QuestionnaireRepondu questionnaire = questionnaireReponduService.validateQuestionnaire(idQuestionnaire);
+
+    if (questionnaire == null) {
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    return new ResponseEntity<>(questionnaire, HttpStatus.OK);
+  }
+
+  @PatchMapping("/questionnaires/finish/{idQuestionnaire}")
+  public ResponseEntity<QuestionnaireRepondu> finishQuestionnaire(@PathVariable String idQuestionnaire) {
+    QuestionnaireRepondu questionnaire = questionnaireReponduService.finishQuestionnaire(idQuestionnaire);
 
     if (questionnaire == null) {
       return new ResponseEntity<>(HttpStatus.NOT_FOUND);
