bc9d21c70502915f9186966e18df9cb4fb49da6d
Robert <robert.ailenei@student.vinci.be>
2024-12-08 14:20:29 +0100

    scroll trigger for 3rd container done

--- frontEnd/src/app/homepage/homepage.component.ts
+++ frontEnd/src/app/homepage/homepage.component.ts
@@ -19,63 +19,104 @@ export class HomepageComponent implements AfterViewInit {
   
   ngAfterViewInit(): void {
     gsap.registerPlugin(ScrollTrigger);
-    if(isPlatformBrowser(this.platformId)){
+  
+    if (isPlatformBrowser(this.platformId)) {
+      // Animation pour ".jaune"
+     
+     
+
+     
       gsap.to(".jaune h1", {
-        x:"-15vw",
-        y:0,
-        opacity:1,
-        duration:2, 
+        x: "-15vw",
+        y: 0,
+        opacity: 1,
+        duration: 2,
        
-      }),
+      });
+  
       gsap.to(".jaune .description-container", {
-        x:"+10vw",
-        y:0,
-        opacity:1,
-        duration:2, 
-      }),
+        x: "+10vw",
+        y: 0,
+        opacity: 1,
+        duration: 2,
+        
+      });
+  
       gsap.to(".jaune .description-container2", {
-        x:"-10vw",
-        y:0,
-        opacity:1,
-        duration:2, 
-      }),
+        x: "-10vw",
+        y: 0,
+        opacity: 1,
+        duration: 2,
+        
+      });
+  
       gsap.to(".jaune .description-container3", {
-        x:"+10vw",
-        y:0,
-        opacity:1,
-        duration:2, 
-      }),
+        x: "+10vw",
+        y: 0,
+        opacity: 1,
+        duration: 2,
+        scrollTrigger: {
+          trigger: ".jaune .description-container3",
+          start: "top 95%",
+          end: "bottom 95%",
+          toggleActions: "play pause none none",
+        },
+      });
+
+  
+      // Animation pour ".vertFonce"
       gsap.to(".vertFonce .description-container", {
-        x:"-10vw",
-        y:0,
-        opacity:1,
-        duration:2, 
-      }),
+        x: "-10vw",
+        y: 0,
+        opacity: 1,
+        duration: 2,
+        scrollTrigger: {
+          trigger: ".vertFonce .description-container",
+          start: "center center",
+          end: "bottom center",
+          toggleActions: "play none none none",
+        },
+      });
+  
       gsap.to(".vertFonce .environnement-container", {
-        x:"10vw",
-        y:0,
-        opacity:1,
-        duration:2, 
-      }),
+        x: "10vw",
+        y: 0,
+        opacity: 1,
+        duration: 2,
+        scrollTrigger: {
+          trigger: ".vertFonce .environnement-container",
+          start: "top 80%",
+          end: "bottom 60%",
+          toggleActions: "play none none none",
+        },
+      });
+  
       gsap.to(".vertFonce .environnement-container2", {
-        x:"-10vw",
-        y:0,
-        opacity:1,
-        duration:2, 
-      }),
-      gsap.to(".vertFonce .environnement-container3", {
-        x:"10vw",
-        y:0,
-        opacity:1,
-        duration:2, 
-      }),
+        x: "-10vw",
+        y: 0,
+        opacity: 1,
+        duration: 2,
+        scrollTrigger: {
+          trigger: ".vertFonce .environnement-container2",
+          start: "top 80%",
+          end: "bottom 60%",
+          toggleActions: "play none none none",
+        },
+      });
+  
       gsap.to(".bubble .container", {
-        x:0,
-        y:"7vw",
-        opacity:1,
-        duration:2, 
-      })
-    }  
+        x: 0,
+        y: "7vw",
+        opacity: 1,
+        duration: 2,
+        scrollTrigger: {
+          trigger: ".bubble .container",
+          start: "top 80%",
+          end: "bottom 60%",
+          toggleActions: "play none none none",
+        },
+      });
+    }
   }
   
   
