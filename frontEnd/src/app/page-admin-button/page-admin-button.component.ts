9e111a3c9315cfe7c088dc38a3eefbc7a53d140e
Robert <robert.ailenei@student.vinci.be>
2024-12-09 13:07:34 +0100

    did some buttons

---
+++ frontEnd/src/app/page-admin-button/page-admin-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-page-admin-button',
+  imports: [RouterModule],
+  templateUrl: './page-admin-button.component.html',
+  styleUrl: './page-admin-button.component.css'
+})
+export class PageAdminButtonComponent {
+
+}
