1367cc60d4ec8f110f14b5177cd12881e5a693dd
Steedlan <arnaud.schellekens@student.vinci.be>
2024-12-10 15:34:44 +0100

    sauvegarder reponse

--- frontEnd/src/app/questionnaires/question/question.component.ts
+++ frontEnd/src/app/questionnaires/question/question.component.ts
@@ -2,14 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../../margins/header/header.component';
 import { FooterComponent } from '../../margins/footer/footer.component';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { QuestionnaireManagerComponent } from '../questionnaire-manager/questionnaire-manager.component';
-import { QuestionnaireDetail, QuestionRepondus } from '../../services/client.service';
+import { ClientService, QuestionnaireDetail, QuestionRepondus, ReponseRepondue } from '../../services/client.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { concatMap, from, last } from 'rxjs';
 
 
 @Component({
   selector: 'app-question',
-  imports: [HeaderComponent, FooterComponent, CommonModule, QuestionnaireManagerComponent],
+  imports: [HeaderComponent, FooterComponent, CommonModule, QuestionnaireManagerComponent, FormsModule],
   templateUrl: './question.component.html',
   styleUrls: ['./question.component.css'],
 })
@@ -18,7 +20,7 @@ export class QuestionComponent implements OnInit {
   question!: QuestionRepondus;
   isLoading = true;
 
-  constructor(private route: ActivatedRoute, private router : Router) {}
+  constructor(private route: ActivatedRoute, private router : Router, private clientservice: ClientService) {}
 
   ngOnInit(): void {
     const questionId = this.route.snapshot.paramMap.get('id');
@@ -50,13 +52,50 @@ export class QuestionComponent implements OnInit {
     }, 700);
   }
 
-  navigateToNextQuestion(): void {
-    let indexOfCurrentQuestion = this.questionnaire[0].questionsRepondues.indexOf(this.question);
-    indexOfCurrentQuestion++;
-    if (indexOfCurrentQuestion >= this.questionnaire[0].questionsRepondues.length) {
-      indexOfCurrentQuestion = 0;
-    }
-    this.router.navigate(['/esg/question/', this.questionnaire[0].questionsRepondues[indexOfCurrentQuestion].id]);
+  navigateToNextQuestion(question: QuestionRepondus): void {
+    // sauvegarder
+    from(question.reponseRepondus).pipe(
+      // concatMap va s'assurer que chaque appel se fait après la complétion du précédent
+      concatMap(reponse => this.clientservice.changerReponseQuestion(
+        reponse.id,
+        reponse.selectionne,
+        reponse.estEngage
+      )),
+      last(),
+      concatMap(() => this.clientservice.changerCommentaireQuestion(question.id.toString(), question.commentaire )),
+    ).subscribe({
+      next: (data) => {
+        console.log('Réponse modifiée avec succès pour', data.id, data.intitule, 'en', data.selectionne);
+      },
+      error: (err) => {
+        console.error('Erreur lors de la modification de la réponse :', err);
+      },
+      complete: () => {
+        let indexOfCurrentQuestion = this.questionnaire[0].questionsRepondues.indexOf(this.question);
+        indexOfCurrentQuestion++;
+        if (indexOfCurrentQuestion >= this.questionnaire[0].questionsRepondues.length) {
+          indexOfCurrentQuestion = 0;
+        }
+        this.router.navigate(['/esg/question/', this.questionnaire[0].questionsRepondues[indexOfCurrentQuestion].id])
+        .then(() => {
+          window.location.reload();
+        });
+      }
+    });
+
   }
-  
+
+  selectAnswer(reponse : ReponseRepondue): void {
+    reponse.selectionne = !reponse.selectionne;
+  }
+
+  selectEngagement(reponse : ReponseRepondue, event: Event): void {
+    // TODO engagement forcé
+    /* event.preventDefault();
+    event.stopImmediatePropagation();
+    (event.target as HTMLInputElement).checked = reponse.estEngage;
+    return;
+    */
+    reponse.estEngage = event.target ? (event.target as HTMLInputElement).checked : false;
+  } 
 }
